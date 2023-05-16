09f0d7a78bc7e8513176c98aadd27428d853f095
Jakub <j.solarek@wp.pl>
2023-05-16 08:27:54 +0200

    all done

--- script.js
+++ script.js
@@ -19,3 +19,27 @@ window.onscroll = () => {
         }
     });
 }
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    }));
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
+
+function project1(){
+    location.href="https://github.com/novy213/todo";
+}
+function project2(){
+    location.href="https://github.com/novy213/chat-app";
+}
+function project3(){
+    location.href="https://github.com/novy213/tic-tac-toe-online";
+}
